4714f3d1108a887f4378174f2482c0f576d33329
www-data <www-data@localhost>
2024-11-19 12:50:47 +0100

    Added Quill rich-text-editor on blog

--- src/Storefront/Controller/BlogController.php
+++ src/Storefront/Controller/BlogController.php
@@ -298,6 +298,14 @@ class BlogController extends StorefrontController
         // Make table of content
         $description = $blogPost->getDescription();
         
+        // Replace all occurrences of '%5BCLONE%5D' with ':'
+        $description = str_replace('[CLONE]', ':', $description);
+        $description = str_replace('%5BCLONE%5D', ':', $description);
+        $description = str_replace('%5BSEMICLONE%5D', ';', $description);
+        $description = str_replace('%5BCOMMA%5D', ',', $description);
+
+        // dd($description);
+
         // generate table of content
         $contentWithToc = $this->makeToc($description);
 
